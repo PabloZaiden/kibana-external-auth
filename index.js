0019778bfd4ddc9c21a03b8fc4b9c774e793b12b
Pablo Zaidenvoren <pabloz@lagash.com>
2016-06-17 13:24:12 -0300

    Initialize Kibana Plugin (generator-kibana-plugin v1.0.1)

---
+++ index.js
@@ -0,0 +1,30 @@
+import exampleRoute from './server/routes/example';
+
+export default function (kibana) {
+  return new kibana.Plugin({
+    require: ['elasticsearch'],
+
+    uiExports: {
+      app: {
+        title: 'Auth Plugin',
+        description: 'External auth for kibana',
+        main: 'plugins/auth_plugin/app'
+      },
+      hacks: [
+        'plugins/auth_plugin/hack'
+      ]
+    },
+
+    config(Joi) {
+      return Joi.object({
+        enabled: Joi.boolean().default(true),
+      }).default();
+    },
+
+    init(server, options) {
+      // Add server routes and initalize the plugin here
+      exampleRoute(server);
+    }
+
+  });
+};
